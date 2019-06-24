9a21b95ada4b7ed59e4fef678f8f4305f49502f4
Furkan Sezgin <samp_leon@hotmail.com>
2019-06-25 00:08:23 +0300

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php
+//File path
+foreach (__AutoLoad('inc/') as $value) {
+    require_once ($value);
+}
+function __AutoLoad($Pathe) {
+    $path = realpath($Pathe);
+    static $files = [];
+    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+    foreach ($objects as $name => $object) {
+        if (pathinfo($name, PATHINFO_EXTENSION) == 'php') $files[] = $name;
+    }
+    return $files;
+}
+?>
